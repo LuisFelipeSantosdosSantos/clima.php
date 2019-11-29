346e257763eeee1c29316c5ff1a5323c6fb2ddb6
Aluno <Aluno@sn611>
2019-11-29 20:51:45 -0200

    aula dia 29/11

--- index.php
+++ index.php
@@ -6,15 +6,47 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <title>TODO supply a title</title>
+        <title><!--climas --></title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         
         <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="crossorigin="anonymous"></script>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-        
         <script src="js/clima.js"></script>
+        
+            <!-- Tags Gerais -->
+        <meta NAME="DESCRIPTION" CONTENT="Site de clima de Brusque feito por Luis Felipe"/>
+        <meta NAME="ABSTRACT" CONTENT="Climas de Brusque"/>
+        <meta name="keywords" content="Clima ,Temperatura ,Brusque ,temperatura em Brsuque ,tempo para Brusque , vento, Previsão de Brusque ,Tempo ,sc ,santa catarina tempo ,santa catarina Clima ,Termometro ,Havan Brusque ,Climax "/>
+        <meta NAME="title" CONTENT="Informaçoes climaticas" />
+        <meta NAME="identifier-url" content="https://luis-clima-senai.herokuapp.com/" />
+        <meta NAME="author" content="Luis Felipe Santos" />
+        <meta NAME="ROBOTS" CONTENT="All" />
+        <meta NAME="RATING" CONTENT="general" />
+        <meta NAME="DISTRIBUTION" CONTENT="global" />
+        <meta NAME="LANGUAGE" CONTENT="pt-br" />
+        <meta name="content-language" content="portuguese" />
+        <meta name="doc-class" content="Completed" /> 
+        <meta name="reply-to" content="luis_Fs_Santos@estudante.sc.senai.br"/>
+        
+        <!-- Tags Para rede social Facebook -->
+        <meta property="og:url" content="https://luis-clima-senai.herokuapp.com/" />
+        <meta property="og:locale" content="pt_BR" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Informaçoes climaticas" />
+        <meta property="og:title" content="Informaçoes climaticas" />
+        <meta property="og:description" content="Site de clima de Brusque feito por Luis Felipe" />
+        <meta property="og:image" content="https://luis-clima-senai.herokuapp.com/img/icones/umidade.gif" />
+        <meta property="og:image:width" content="400" />
+        <meta property="og:image:height" content="120" />
+        <meta property="fb:app_id" content="" />
+        
+        <!-- Tags Para rede social Twitter -->
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:description" content="Site de clima de Brusque feito por Luis Felipe" />
+        <meta name="twitter:title" content="Informaçoes climaticas" />
+        
     </head>
     
     <body>
